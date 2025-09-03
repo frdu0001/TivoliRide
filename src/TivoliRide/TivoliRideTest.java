b3207a8a3fa41bc267ca15c6f0892a8f930be681
Frederick <frdu0001@stud@ek.dk>
2025-09-03 11:46:17 +0200

    IntilizeRepostory

--- src/TivoliRide/TivoliRideTest.java
+++ src/TivoliRide/TivoliRideTest.java
@@ -1,4 +1,22 @@
 package TivoliRide;
 
-public class TivoliRideTest {
+  public static void TivoliRideTest {
+
+  OpretRide();
 }
+  Public static void OpretRide(); {
+      Ride r1 = new TivoliRide()
+              UdskriveRide(r1);
+          }
+public static Void UdskriveRide (Ride r1) {
+      System.out.println();
+      if (r1.Height() >= 150) {
+          System.out.println("Sorry you not tall enough for this ride");
+      }
+      else {
+          System.out.println("Enjoy the ride");
+      }
+}
+
+
+
